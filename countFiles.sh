8c3e0cf8691736b80ac9f0caa2ef35a238ed3009
Tanishq Gupta <tanishqg2406@gmail.com>
2017-12-17 14:35:19 +0530

    Completed

--- countFiles.sh
+++ countFiles.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 count=0
-if [ $# -gt 2 ] || [ $# -lt 1 ]; then
+
+if [ $# -gt 2 ] || [ $# -lt 1 ] || [ ! -d $1 ] || [ ! -r $1 ]; then
 	exit 1
 elif [ $# = 1 ]; then
-	if [ ! -d "$1" ]; then
-		exit 1;
-	else
-		for file in $1/*; do
-			if [ -f "$file" ]; then
-				count=$((count+1))
-			fi
-		done
-		echo $count
-	fi
+	for file in $1/*; do
+		if [ -f "$file" ]; then
+			count=$((count+1))
+		fi
+	done
 else
-	exit 0	
+	for file in $1/*$2; do
+		if [ -f "$file" ]; then
+			count=$((count+1))
+		fi
+	done
 fi
+
+echo $count
+exit 0
