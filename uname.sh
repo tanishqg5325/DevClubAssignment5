983f03025acf225ddc095027b4f3cc57335d8dcb
Tanishq Gupta <tanishqg2406@gmail.com>
2017-12-17 22:20:41 +0530

    completed

--- uname.sh
+++ uname.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-if [ ! $# = 2 ]  || [ ! -f $1 ] || [ ! $1 = *.txt ] || [ ! -r $1 ]; then
+if [ ! $# = 2 ] || [ ! -f $1 ] || [ ! -r $1 ] || [[ ! "$1" = *.txt ]]; then
 	exit 1
 else
 	file=$1
 	uname=$2
-	
+	grep "$uname:x:*:*:*:*" $file>abc.txt
+	if [ ! -s abc.txt ]; then
+		echo "Invalid Username"
+		exit 1
+	fi
+	cut -d ":" -f 5 abc.txt
+	rm -f abc.txt
 fi
 
 exit 0
