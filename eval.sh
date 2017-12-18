17624c8b5bfd3ef940cba00f95616e2df37ae584
Tanishq Gupta <tanishqg2406@gmail.com>
2017-12-18 12:11:52 +0530

    completed along with checking input format

--- eval.sh
+++ eval.sh
@@ -3,7 +3,24 @@
 if [ ! $# = 1 ]  || [ ! -f $1 ] || [ ! -r $1 ] || [[ ! "$1" = *.txt ]]; then
 	exit 1
 else
-	num=0;
+	num1=0
+	while read line; do
+		if [[ ! "$line" =~ ^-?[0-9]+' '.$ ]]; then
+			exit 1;
+		fi
+		num2=$(echo "$line" | cut -d " " -f 1)
+		op=$(echo "$line" | cut -d " " -f 2)
+		
+		case "$op" in
+			"+") let num1+=num2;;
+			"-") let num1-=num2;;
+			"*") let num1*=num2;;
+			"/") let num1/=num2;;
+			*) exit 1;; 
+		esac
+
+	done<$1
+	echo $num1;
 fi
 
 exit 0
