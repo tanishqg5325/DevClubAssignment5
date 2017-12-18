8c6d36aaa7f8e7c0dfcad30be87b779600c24e14
Tanishq Gupta <tanishqg2406@gmail.com>
2017-12-18 16:21:05 +0530

    Directories synced properly but unable to correctly list the changes

--- backup.sh
+++ backup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+sync_dir(){
+	for file in $(ls $2); do
+		rm -r temp1/$file
+	done
+	ls -1 temp1/
+	rm -r temp1
+	cp -r final temp1
+}
+
 if [ ! $# = 2 ] || [ ! -d $1 ] || [ ! -d $2 ]; then
 	exit 1
 else
@@ -8,35 +17,18 @@ else
 	cp -r $2 temp1
 	cp -r temp1/dir2/* temp1
 	rm -r temp1/dir2
-	cp -r temp1 temp2
 	cp -r temp1 final
+
 	echo "Files copied from $1 to $2 are:"
-	cd $2
-	for file in *; do
-		cd $temp
-		rm -r temp1/$file
-		cd $1
-	done
-	cd $temp
-	ls -1 temp1/
-	rm -r temp1
+	sync_dir $1 $2
 	
 	echo "Files copied from $2 to $1 are:"
-	cd $1
-	for file in *; do
-		cd $temp
-		rm -r temp2/$file
-		cd $1
-	done
-	cd $temp
-	ls -1 temp2/
-	rm -r temp2
-	rm -r $1
-	rm -r $2
+	sync_dir $2 $1
+
+	rm -r $1 $2 temp1
 	cp -r final $1
 	cp -r final $2
 	rm -r final
-
 fi
 
 exit 0
